4fe762f935c7e61f3258b686375c31533730ee41
Diego Lopes <diego95lopes@live.com>
2026-06-22 14:51:38 -0400

    Disable annoying warnings

--- qr.cpp
+++ qr.cpp
@@ -1,2 +1,11 @@
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4244 4267) // vendored qr.hpp: narrowing size_t/long → int/uint16_t
+#endif
+
 #define QR_IMPLEMENTATION
 #include "qr.hpp"
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
